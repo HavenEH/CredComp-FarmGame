d0ee15675de6c54f3ac6b1db6c1f5c9fb59e76b9
HavenEH <159040819+HavenEH@users.noreply.github.com>
2024-06-19 18:01:34 +0000

    Initial commit

---
+++ map.py
@@ -0,0 +1,3 @@
+#farmhouse | Barn | Field | Field | Field | Field | Field | Field | Field
+def mapimage(): 
+  print("| farmhouse | Barn  | Field |\n| Field     | Field | Field |\n| Field     | Field | Field |")
---
+++ player.py
@@ -0,0 +1,12 @@
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.inventory = []
+
+    def add_to_inventory(self, item):
+        self.inventory.append(item)
+      
+    def 
+  
+  
